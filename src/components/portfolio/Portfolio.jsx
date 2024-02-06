3ff1019281f8d66d9625338f9ed11ab8b553ba19
Aaqil Khan <en21cs3l1003@medicaps.ac.in>
2024-02-06 13:28:06 +0530

    first comit

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,93 @@
+import { useRef } from "react";
+import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    title: "Masterly Education",
+    img: "./masterly.jpg",
+    desc: "The “Student Community Application” project is an innovative educational technology initiative that acknowledges the diverse aspects of the modern student experience beyond traditional classroom learning, as explored in this detailed report which delves into the project’s goals, approaches, and the importance of its key features—the “Mock Test” and “Resume Builder” modules.",
+    link: "https://abdulaaqilkhan.github.io/MasterlyLanding/",
+  },
+  {
+    id: 2,
+    title: "Hinalicious portfolio",
+    img: "./hina.jpg",
+    desc: "Hina Afreen an Islamic teacher wanted an amazing portfolio that clould stand our from the row of ordinary so i created this master piece for her.",
+    link: "https://hinalicious.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Feedants Social",
+    img: "./feedweb.jpg",
+    desc: "I am also working in Blaccskull pvt. ltd. as an Full-time intern, currently i am working on this social media web site and its android/Ios application simultenously.",
+    link: "https://feedants.com/#/",
+  },
+  {
+    id: 4,
+    title: "Matrix.co",
+    img: "./matrix.jpg",
+    desc: "Matrix .co is my own project which aims to deliver some premimum services to students it is an extension the Masterly, Matrix objecty the tech related problems faced by students and tend to solve them in an easy manner & cost free.",
+    link: "https://matrixv1.vercel.app/",
+  },
+];
+
+const Single = ({ item }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section >
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{y}}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <a href={item.link} target="_blank">
+              <button>
+                See Demo
+              </button>
+            </a>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Portfolio = () => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Live Projects</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Portfolio;
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,104 @@
+import { useRef } from "react";
+import "./services.scss";
+import { motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const Services = () => {
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
+
+  return (
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      // animate="animate"
+      // whileInView="animate"
+      ref={ref}
+      animate={"animate"}
+    >
+      <motion.div className="textContainer" variants={variants}>
+        <p>
+          I focus on helping your brand grow
+          <br /> and move forward
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer" variants={variants}>
+        <div className="title">
+          <img src="./people.webp" alt="" />
+          <h1>
+            <motion.b whileHover={{color:"orange"}}>Unique</motion.b> Ideas
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <motion.b whileHover={{color:"orange"}}>For Your</motion.b> Business.
+          </h1>
+          <button>Stack i work on.</button>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>React.js</h2>
+          <p>
+          React is a JavaScript library created by Facebook. It’s the most popular library for building single-page applications and interactive user interfaces. Interestingly enough, oftentimes React is referred to as a framework.
+          </p>
+          <button>Read more...</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Express.js</h2>
+          <p>
+          Express.js is a small framework that works on top of Node.js web server functionality to simplify its APIs and add helpful new features. It makes it easier to organize your application’s functionality with middleware and routing.
+          </p>
+          <button>Read more...</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Node.js</h2>
+          <p>
+          Node JS is an open-source and cross-platform runtime environment built on Chrome’s V8 JavaScript engine for executing JavaScript code outside of a browser. It provides an event-driven, non-blocking (asynchronous) I/O and cross-platform runtime environment for building highly scalable server-side applications using JavaScript.
+          </p>
+          <button>Read more...</button>
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{ background: "lightgray", color: "black" }}
+        >
+          <h2>Mongo DB</h2>
+          <p>
+          MongoDB is an open-source document-oriented database that is designed to store a large scale of data and also allows you to work with that data very efficiently. It is categorized under the NoSQL (Not only SQL) database because the storage and retrieval of data in the MongoDB are not in the form of tables. 
+          </p>
+          <button>Read more...</button>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Services;
